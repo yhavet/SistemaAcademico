82a5df266e70a27d004109dd927fefd1e039ca08
Yhavet <alnzyhav@gmail.com>
2024-09-25 23:03:52 -0300

    CAMBIOS

--- SistemaEscolar3/SistemaEscolar3/AñadirDatosDocente.cs
+++ SistemaEscolar3/SistemaEscolar3/AñadirDatosDocente.cs
@@ -10,10 +10,10 @@ namespace SistemaEscolar3
 {
     class AñadirDatosDocente
     {
-        SqlConnection connect = new SqlConnection(@"");
+        SqlConnection connect = new SqlConnection("Data Source=YHAVET\\SQLEXPRESS;Initial Catalog=Tecnica3;Integrated Security=True;Connect Timeout=30");
 
         public int id { set; get; }
-        public string Iddocente{ set; get; }
+        public string IdDocente{ set; get; }
         public string NombreDocente { set; get; }
         public string GeneroDocente { set; get; }
         public string DireccionDocente { set; get; }
@@ -21,10 +21,46 @@ namespace SistemaEscolar3
         public string StatusDocentes { set; get; }
         public string CursosDocentes { set; get; }
 
-        //public List<AñadirDatosDocente> DatosDocentes()
-        //{
-        //    List<AñadirDatosDocente> listData = new List<AñadirDatosDocente> ();
-        //}
+        public List<AñadirDatosDocente> DatosDocentes()
+        {
+            List<AñadirDatosDocente> listData = new List<AñadirDatosDocente>();
+
+            if (connect.State != ConnectionState.Open)
+            {
+                try
+                {
+                    connect.Open();
+
+                    string SQL = "Select * from docentes WHERE date_delete IS NULL";
+                    using(SqlCommand cmd = new SqlCommand(SQL, connect))
+                    {
+                        SqlDataReader reader = cmd.ExecuteReader();
+
+                        while(reader.Read()) 
+                        { 
+                            id = (int)reader["id"];
+                            IdDocente = reader["id_docente"].ToString();
+                            NombreDocente = reader["id_docente"].ToString();
+                            GeneroDocente = reader["genero_docente"].ToString();
+                            DireccionDocente = reader["direccion_docente"].ToString();
+
+                        }
+                        reader.Close();
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error al conectarse a la base de datos: " + ex);
+                }
+                finally
+                {
+                    connect.Close();
+                }
+
+            }
+            
+        }
 
 
 
